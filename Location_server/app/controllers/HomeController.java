aa490207734e86b6a29b85a830d1b6a1a2a3100e
ylu36 <ylu36@ncsu.edu>
2018-08-27 12:57:33 -0400

    calculate distance in Km using Haversine formula

--- Location_server/app/controllers/HomeController.java
+++ Location_server/app/controllers/HomeController.java
@@ -16,6 +16,8 @@ import com.fasterxml.jackson.databind.JsonNode;
  */
 public class HomeController extends Controller {
 
+    private static final double d2r = Math.PI / 180.;
+
     @Inject        
     DatabaseController databaseController;
     // FormFactory formFactory;
@@ -61,4 +63,16 @@ public class HomeController extends Controller {
         JsonNode locationJson = Json.toJson(location);
         return ok(locationJson);
     }
+
+    // ref: https://en.wikipedia.org/wiki/Haversine_formula
+    public double calculateDistanceInKm(double lat1, double long1, double lat2, double long2) {
+        double dlong = (long2 - long1) * d2r;
+        double dlat = (lat2 - lat1) * d2r;
+        double a = Math.pow(Math.sin(dlat/2.0), 2) + Math.cos(lat1*d2r) * Math.cos(lat2*d2r) * Math.pow(Math.sin(dlong/2.0), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double d = 6367 * c;
+
+        return d;
+    }
+
 }
