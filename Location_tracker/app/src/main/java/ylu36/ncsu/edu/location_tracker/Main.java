88fec616f41eb79d3f0bd899fa7ef2b153809d63
Yuanchen Lu <ylu36@ncsu.edu>
2018-08-29 22:06:11 -0400

    can post to server

--- Location_tracker/app/src/main/java/ylu36/ncsu/edu/location_tracker/Main.java
+++ Location_tracker/app/src/main/java/ylu36/ncsu/edu/location_tracker/Main.java
@@ -1,16 +1,15 @@
 package ylu36.ncsu.edu.location_tracker;
 
-import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.*;
+import android.util.Log;
+
 import android.os.Bundle;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.location.Location;
 import android.location.LocationManager;
 import android.location.LocationListener;
@@ -18,11 +17,21 @@ import android.location.Criteria;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import android.widget.TextView;
+import android.graphics.Color;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONObject;
 
 public class Main extends AppCompatActivity {
     static final int MY_PERMISSIONS_REQUEST_CONST = 1;
     EditText hostField, usernameField;
     Button btn;
+    TextView resultView;
     LocationManager locationManager;
     LocationListener locationListener;
     String provider;
@@ -32,26 +41,12 @@ public class Main extends AppCompatActivity {
     public void getLocation(final Location location) {
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
-        btn.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                String username = usernameField.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(),username,Toast.LENGTH_LONG);
-                msg.show();
-//                updateLocation(location, username);
-            }
-        });
+        String username = String.valueOf(usernameField.getText());
+        String str = username + "'s location Changed New Location is: " + "Latitude: " + latitude + " Longitude: " + longitude;
+        resultView.setText(str);
+        sendRequest(location);
     }
 
-    public static boolean hasPermissions(Activity activity, String[] permissions) {
-       boolean flag = true;
-       for (String permission : permissions) {
-                if (ContextCompat.checkSelfPermission(activity, permission) != PackageManager.PERMISSION_GRANTED) {
-                    flag = false;
-                    break;
-                }
-       }
-       return flag;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +54,8 @@ public class Main extends AppCompatActivity {
         hostField = findViewById(R.id.editText);
         usernameField = findViewById(R.id.editText2);
         btn = findViewById(R.id.button);
+        resultView = (TextView) findViewById(R.id.textView);
+        resultView.setText("");
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(final Location location) {
@@ -85,28 +82,90 @@ public class Main extends AppCompatActivity {
         criteria.setCostAllowed(true);
         criteria.setPowerRequirement(Criteria.POWER_LOW);
         provider = locationManager.getBestProvider(criteria, false);
-        if(provider != null) {
-            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                locationManager.requestLocationUpdates(provider, 1000, 10, locationListener);
-                //btn.setText(R.string.pause);
-                Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
-            }
-        }
-        btn.setOnClickListener(new OnClickListener() {
-            public void onClick(View v) {
-                String str = hostField.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
-                msg.show();
-            }
-        });
 
+        btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               if(provider != null) {
+                   if(ContextCompat.checkSelfPermission(Main.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                       locationManager.requestLocationUpdates(provider, 1000, 10, locationListener);
+                   }
+               }
+           }
+       });
+//        btn.setOnClickListener(new OnClickListener() {
+//            public void onClick(View v) {
+//                String str = hostField.getText().toString();
+//                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+//                msg.show();
+//            }
+//        });
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (ActivityCompat.checkSelfPermission(Main.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            //register the Location Listener with Location Manager
+//            locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Un-registering Location Listener.
+        locationManager.removeUpdates(locationListener);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        if (!hasPermissions(this, permissions)) {
-            ActivityCompat.requestPermissions(this, permissions, MY_PERMISSIONS_REQUEST_CONST);
+
+//        btn.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String oldResult = String.valueOf(resultView.getText());
+//                // resultView.setText(("\nStarted Tracking"));
+//                if (ActivityCompat.checkSelfPermission(Main.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+//                    locationManager.requestLocationUpdates(provider, 1000, 1, locationListener);
+//                }
+//                else {
+//                    resultView.setText(("\nStopped Tracking"));
+//                }
+//            }
+//        });
+    }
+
+    private void sendRequest(Location location) {
+        String host = String.valueOf(hostField.getText());
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://" + host + "/locationupdate";
+        JSONObject json = new JSONObject();
+        try {
+            json.put("username", usernameField.getText());
+            json.put("latitude", location.getLatitude());
+            json.put("longitude", location.getLongitude());
+            json.put("timestamp", System.currentTimeMillis());
+        }catch(Exception e){
+            Log.e("Cannot create json",e.getMessage());
         }
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url, json, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.i("successful request", "Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Unsuccessful request",error.getMessage());
+                        resultView.setText("Could not connect to server!");
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        queue.add(jsonObjectRequest);
     }
 }
